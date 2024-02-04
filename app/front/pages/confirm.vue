82f60d0310e8e61f863a5aabe2441678701da5f3
yusuke.honda <honsuke.neko.nyan0615@gmail.com>
2024-02-04 20:57:01 +0900

    ref #35 認証コード再送機能を実装

--- app/front/pages/confirm.vue
+++ app/front/pages/confirm.vue
@@ -2,27 +2,36 @@
 	<div>
 		<Alert ref="alert" />
 		<div class="my-10">
-				<v-row justify="center">
-				<v-col cols="12" lg="4" sm="6">
-				<v-card class="pa-5">
+      <v-row justify="center">
+				<v-col cols="12" lg="6" sm="6">
+					<v-card class="pa-5">
 						<v-card-title class="d-flex justify-center mb-3">
-						<div>
+							<div>
 								<v-img :src="`/logo-no-background.png`" contain height="180"></v-img>
-						</div>
+							</div>
 						</v-card-title>
 						<v-form ref="confirmForm" lazy-validation>
-						<v-text-field
-								v-model="code"
-								label="verification code"
-								:rules="[rules.required]"
-						></v-text-field>
-						<div class="d-flex justify-end">
-								<v-btn color="secondary" class="mr-4" type="submit" @click.prevent="confirmSignUp">confirm</v-btn>
-						</div>
+							<v-text-field
+									v-model="code"
+									label="verification code"
+									:rules="[rules.required]"
+							></v-text-field>
+							<v-row>
+								<v-col cols="12" lg="6" sm="6" class="d-flex justify-start">
+									<div>
+											<v-btn color="info" class="mr-4" type="submit" @click.prevent="resendConfirmationCode">resend code</v-btn>
+									</div>
+								</v-col>
+								<v-col cols="12" lg="6" sm="6" class="d-flex justify-end">
+									<div>
+											<v-btn color="error" class="mr-4" type="submit" @click.prevent="confirmSignUp">confirm</v-btn>
+									</div>
+								</v-col>
+							</v-row>
 						</v-form>
-				</v-card>
+					</v-card>
 				</v-col>
-				</v-row>
+			</v-row>
 		</div>
 	</div>
 </template>
@@ -62,4 +71,22 @@ async function confirmSignUp() {
 		}, 2000)
 	}
 }
+
+// 認証コードを再送
+async function resendConfirmationCode() {
+  const { data, pending, error, refresh } = await useAsyncData<any>(
+    "resendConfirmationCode",
+    () => {
+      Auth.resendSignUp(username)
+    }
+  )
+  if (error.value) {
+    console.error(error.value)
+		alert.value.error(error.value)
+    return
+  } else {
+		alert.value.success("認証コードを再送しました")
+	}
+}
+
 </script>
