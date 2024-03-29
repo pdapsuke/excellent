210627e4c6b2ccf88d865c3d3751243a53536956
yusuke.honda <honsuke.neko.nyan0615@gmail.com>
2024-03-29 20:52:27 +0900

    refactor_shell_commands image_push.shを簡潔に修正

--- bin/image-push.sh
+++ bin/image-push.sh
@@ -3,19 +3,21 @@
 function usage {
 cat <<EOF >&2
 [USAGE]
-  $0 --stage <STAGE>
+  $0 -s | --stage <STAGE> -t | --tag <IMAGE_TAG>
+  $0 -s stg -t latest
 EOF
 exit 1
 }
-
 PROJECT_ROOT="$(cd $(dirname $0)/..; pwd)"
 cd "$PROJECT_ROOT"
 
 STAGE=
+IMAGE_TAG=
 args=()
 while [ "$#" != 0 ]; do
   case $1 in
     -s | --stage ) shift; STAGE="$1" ;;
+    -t | --tag   ) shift; IMAGE_TAG="$1" ;;
     -* | --*     ) echo "不正なオプション: $1" >&2; exit 1 ;;
     *            ) args+=("$1") ;;
   esac
@@ -24,33 +26,30 @@ done
 
 [ "${#args[@]}" != 0 ] && usage
 [ -z "$STAGE" ] && usage
+[ -z "$IMAGE_TAG" ] && usage
 
 set -e
 
+# プッシュ先のリポジトリ名を定義
 REPOSITORY_NAME="excellent/${STAGE}/app"
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 AWS_REGION="ap-northeast-1"
-
-
-# イメージビルド
-docker build --rm -f docker/front/Dockerfile -t ${REPOSITORY_NAME}-front:latest .
-docker build --rm -f docker/api/Dockerfile -t ${REPOSITORY_NAME}-api:latest .
-docker build --rm -f docker/nginx/Dockerfile -t ${REPOSITORY_NAME}-nginx:latest .
+REMOTE_REPOSITORY_PREFIX="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY_NAME}"
 
 # ECRにログイン
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
-# ECRにフロントイメージをpush
-REMOTE_REPOSITORY_NAME_FRONT=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY_NAME}-front:latest
-docker tag ${REPOSITORY_NAME}-front:latest $REMOTE_REPOSITORY_NAME_FRONT
-docker push $REMOTE_REPOSITORY_NAME_FRONT
+# ビルドとプッシュの手順を関数化
+build_and_push_image() {
+  IMAGE_NAME="$1"
+  DOCKERFILE_PATH="$2"
+  REMOTE_IMAGE_NAME="$REMOTE_REPOSITORY_PREFIX-$IMAGE_NAME:$IMAGE_TAG"
 
-# ECRにapiイメージをpush
-REMOTE_REPOSITORY_NAME_API=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY_NAME}-api:latest
-docker tag ${REPOSITORY_NAME}-api:latest $REMOTE_REPOSITORY_NAME_API
-docker push $REMOTE_REPOSITORY_NAME_API
+  docker build --rm -f "$DOCKERFILE_PATH" -t "$REMOTE_IMAGE_NAME" .
+  docker push "$REMOTE_IMAGE_NAME"
+}
 
-# ECRにnginxイメージをpush
-REMOTE_REPOSITORY_NAME_NGINX=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY_NAME}-nginx:latest
-docker tag ${REPOSITORY_NAME}-nginx:latest $REMOTE_REPOSITORY_NAME_NGINX
-docker push $REMOTE_REPOSITORY_NAME_NGINX
+# Dockerイメージのビルドとプッシュ
+build_and_push_image "front" "docker/front/Dockerfile"
+build_and_push_image "api" "docker/api/Dockerfile"
+build_and_push_image "nginx" "docker/nginx/Dockerfile"
