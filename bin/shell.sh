81b584ed794e65b6763f4940d625a221d0881f69
yusuke.honda <honsuke.neko.nyan0615@gmail.com>
2024-03-30 18:49:23 +0900

    refactor_shell_commands 開発用シェルコンテナを軽量になるように修正

--- bin/shell.sh
+++ bin/shell.sh
@@ -2,7 +2,7 @@
 
 function usage {
 cat >&2 <<EOS
-開発環境でアプリを起動するコマンド
+開発環境でシェルを起動するコマンド
 
 [usage]
  $0 -e ./local.env
@@ -40,8 +40,8 @@ docker build \
   --build-arg host_uid=$USER_ID \
   --build-arg host_gid=$GROUP_ID \
   --rm \
-  -f docker/dev/Dockerfile \
-  -t excellent-app-dev:latest \
+  -f docker/shell/Dockerfile \
+  -t excellent-shell:latest \
   .
 
 LOCAL_APP_DIR="${PROJECT_ROOT}/app"
@@ -51,9 +51,8 @@ docker run \
 	-ti \
 	--network host \
 	--env-file "$ENV_PATH" \
-	-e "DB_NAME=$DB_NAME" \
 	-w /opt/app \
 	--user="$USER_ID:$GROUP_ID" \
 	-v ${LOCAL_APP_DIR}:/opt/app \
-	excellent-app-dev:latest \
+	excellent-shell:latest \
 	"/bin/bash"
--- docker/dev/entrypoint-api.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /opt/app/api
-printenv
-uvicorn main:app --reload --port 8018 --host 0.0.0.0
