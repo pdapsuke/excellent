b6de5a9ab55a9aabf9701bb64a0cebadac0f5ccd
yusuke.honda <honsuke.neko.nyan0615@gmail.com>
2024-04-23 21:36:55 +0900

    refactoring サインアップ確認画面への直アクセス制御方法を修正

--- app/front/middleware/disable-direct-access.ts
+++ app/front/middleware/disable-direct-access.ts
@@ -1,6 +1,7 @@
-// この書き方でURL直アクセスを制限できる理由はあんま分かってない
-export default defineNuxtRouteMiddleware((from, $route) => {
-    if (from.name === $route.name) {
-      return navigateTo('/signin')
-    }
+export default defineNuxtRouteMiddleware((to, from) => {
+  // 遷移前のパスと遷移後のパスが等しい、かつ
+  // クエリパラメータがない場合(更新ボタンを押下時は遷移前後のパスが等しくなるため)、ページへの直接アクセスとみなす
+  if (from.path == to.path && Object.keys(to.query).length === 0) {
+    return navigateTo('/signin')
+  }
 })
